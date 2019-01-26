f48cf1549ec3379abdd5d450205ec1ee6ad3aafb
ianschu <ianschu7@gmail.com>
2019-01-26 14:52:50 -0500

    basic spotify search complete

--- liri.js
+++ liri.js
@@ -1,12 +1,31 @@
 require("dotenv").config();
-
 var keys = require("./keys.js");
-
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-// searches spotify API
-songQuery = "Ocean Man"
-function spotSearch() {
+// checks if search query is string of length greater than 0
+var validQuery = function() {
+  if(isNaN(makeQueryString()) && makeQueryString().length>0){
+    return makeQueryString();
+  }else{
+    // failure results in Rickroll
+    console.log("Invalid Search: Initiate Rickroll\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    return("Never Gonna Give You Up");
+  }
+}
+// function to concatenate arguments after the third into one search
+var  makeQueryString = function() {
+  let queryString = " ";
+  for (i = 3; i < process.argv.length; i++) {
+    queryString = queryString + process.argv[i]+" ";
+  };
+  return queryString.trim();
+};
+// input data variables
+let searchType = process.argv[2];
+let searchQuery = validQuery();
+
+// function searches spotify API and logs values to cmd line
+function spotSearch(songQuery) {
   spotify.search({ type: 'track', query: songQuery }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
@@ -16,14 +35,17 @@ function spotSearch() {
     // }
 
     // console.logs artists
-    console.log("Song Name: "+data.tracks.items[0].name);
-    console.log("Artist: "+data.tracks.items[0].artists[0].name);
-    console.log("Album: "+data.tracks.items[0].album.name);
-    console.log("Preview Link: "+data.tracks.items[0].preview_url);
+    console.log("Song Name: " + data.tracks.items[0].name);
+    console.log("Artist: " + data.tracks.items[0].artists[0].name);
+    console.log("Album: " + data.tracks.items[0].album.name);
+    console.log("Preview Link: " + data.tracks.items[0].preview_url);
 
   });
 };
-spotSearch();
+// checks if user wants Spotify search
+if (searchType === "spotify-this-song") {
+  spotSearch(searchQuery);
+};
 // list of commands to impliment:
 // concert-this
 // spotify-this-song
